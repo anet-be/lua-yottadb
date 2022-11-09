5a33a312e3eb73bfb65289779507ed0dfaa8f190
Berwyn <berhoyt@gmail.com>
2022-11-09 15:11:33 +1300

    Make lock.lua not print 'not locked' instead of confusing 'failed' when it's not a test failure

--- tests/lock.lua
+++ tests/lock.lua
@@ -4,9 +4,9 @@
 local _yottadb = require('_yottadb')
 local ok, e = pcall(_yottadb.lock_incr, arg[1], {arg[2]})
 if ok then
-  print('Lock Success')
+  print('Locked')
 elseif tonumber(e:match('^YDB Error: (%d+)')) == _yottadb.YDB_LOCK_TIMEOUT then
-  print('Lock Failed')
+  print('Not locked')
   os.exit(1)
 else
   print('Lock Error: ' .. e)
@@ -14,4 +14,4 @@ else
 end
 os.execute('sleep 1.5')
 _yottadb.lock_decr(arg[1], {arg[2]})
-print('Lock Released')
+print('Lock released')
