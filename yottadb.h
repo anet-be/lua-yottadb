a1c23c2d5f2935308f90cb677f363163e3123f6d
Berwyn <berhoyt@gmail.com>
2023-04-22 23:14:07 +1200

    Integrated cachearray into yottadb.c; now need to add it to yottadb.lua

--- yottadb.h
+++ yottadb.h
@@ -51,4 +51,42 @@ static __inline__ void *_realloc_safe(void *buf, size_t size, int line, char *fi
   YDB_MALLOC_BUFFER_SAFE((BUFFERP), len_used); \
 }
 
+// Debug macro to dump the Lua stack
+inline static void dumpStack (lua_State *L) {
+  int i;
+  int top = lua_gettop(L); /* depth of the stack */
+  printf("STACK: ");
+  for (i = 1; i <= top; i++) { /* repeat for each level */
+    int t = lua_type(L, i);
+    switch (t) {
+      case LUA_TSTRING: { /* strings */
+        printf("'%s'", lua_tostring(L, i));
+        break;
+      }
+      case LUA_TBOOLEAN: { /* Booleans */
+        printf(lua_toboolean(L, i) ? "true" : "false");
+        break;
+      }
+      case LUA_TNUMBER: { /* numbers */
+        printf("%g", lua_tonumber(L, i));
+        break;
+      }
+      default: { /* other values */
+        printf("%s", lua_typename(L, t));
+        break;
+      }
+    }
+    printf(" "); /* put a separator */
+  }
+  printf("\n"); /* end the listing */
+}
+
+// Debug macro to print a field `fieldstr` of the table at index
+inline static void printField(lua_State *L, int index, char *fieldstr) {
+  lua_getfield(L, index, fieldstr);
+  printf("%s=%s(%s)\n", fieldstr, lua_tostring(L, -1), lua_typename(L, lua_type(L, -1)));
+  fflush(stdout);
+  lua_pop(L, 1);
+}
+
 #endif // LUA_YOTTADB_H
